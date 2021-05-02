89b26184f9b95af64cede68f5e87a69aa1f94593
junsang <gufrus@naver.com>
2021-05-02 21:27:09 +0900

    [2021.05.02]  Member Entity 코드 작성

--- member/src/main/java/com/junsang/member/member/domain/Members.java
+++ member/src/main/java/com/junsang/member/member/domain/Members.java
@@ -1,11 +1,28 @@
 package com.junsang.member.member.domain;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "TB_MEMBER_MASTER")
 public class Members {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column private Long SEQ;
+    @Column(nullable=false)
+    private Long seq;                   // 시퀀스
+
+    @Column(nullable=false)
+    private String nm;                  // 이름
+
+    @Column(nullable=false)
+    private String pw;                  // 패스워드
+
+    @Column(nullable=false)
+    private String email;               // 이메일
+
+    @Column(nullable=false, columnDefinition="char", length=2)
+    private String joinCd;              // 가입 코드
+
+    @Column(nullable=true)
+    private LocalDateTime lastLoginDt;  // 마지막 로그인 일자
 }
